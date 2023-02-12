98f5eec34dc6c6b51a1c9f3e53245866513534d6
Vince <wjs531@gmail.com>
2023-02-12 17:59:23 +0900

    vue3-lifecycle

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>{{ count }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 2,
+    };
+  },
+
+  beforeCreate() {
+    console.log("beforecreat!", this.count);
+  },
+  created() {
+    console.log("created!", this.count);
+  },
+  beforeMount() {
+    console.log("beforemount!");
+  },
+  mounted() {
+    console.log("mounted!");
+    console.log(document.querySelector("h1"));
+  },
+};
+</script>
